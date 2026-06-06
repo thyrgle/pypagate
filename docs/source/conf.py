033a99bf4fcb061782307ce2cacf5ad83caf0715
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-06-06 14:07:32 -0700

    Added sys path extensions for autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,13 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# For autodoc
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path('..', 'src').resolve()))
+
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
